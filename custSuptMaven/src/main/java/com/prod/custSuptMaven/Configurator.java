8fd10f4e9ea6d7f5c6043edfc665a0ce8afda61d
Scott Francis <sgf14@netzero.com>
2016-09-11 14:42:07 -0600

    Added chap 9 -added logging.  log4j implementation

--- custSuptMaven/src/main/java/com/prod/custSuptMaven/Configurator.java
+++ custSuptMaven/src/main/java/com/prod/custSuptMaven/Configurator.java
@@ -3,6 +3,9 @@ package com.prod.custSuptMaven;
 //when new pages created in app you will want to add them the URL list below so they are login protected
 //see chapter 10 (customer-support-v8 project) pg 289 as an example with the websockets chat functionality.
 // the urls are housed in the different XxServlet.java classes for the different pages via the @WebServer annotation.
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -16,8 +19,17 @@ public class Configurator implements ServletContextListener {
 		ServletContext context = event.getServletContext();
 		
 		FilterRegistration.Dynamic registration = context.addFilter(
-				"authenticationFilter", new AuthenticationFilter());
-		registration.setAsyncSupported(true);
+                "loggingFilter", new LoggingFilter()
+        );
+        registration.addMappingForUrlPatterns(
+                EnumSet.of(DispatcherType.REQUEST, DispatcherType.INCLUDE,
+                        DispatcherType.FORWARD, DispatcherType.ERROR),
+                false, "/*"
+        );
+
+        registration = context.addFilter(
+                "authenticationFilter", new AuthenticationFilter()
+        );
 		//if more @WebServlet pages are added just add their URL patterns to the list below
 		//only affects TicketServlet and SessionListener servlets at this time- see pg 255
 		registration.addMappingForUrlPatterns(null, false, "/sessions", "/tickets", "/chat");
